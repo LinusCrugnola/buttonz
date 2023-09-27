2a26e339f0ea453df14e871d30fda6ef24f453cf
LinusCrugnola <linus.crugnola@sourceengineers.com>
2023-09-27 19:44:05 +0200

    add extension

---
+++ README.md
@@ -0,0 +1,34 @@
+# buttonz README
+
+Readme for buttonz extension
+
+## Features
+
+Custom buttons
+
+## Requirements
+
+None
+
+## Extension Settings
+
+You can define the commands that are run by the buttons in settings.json:
+```json
+{
+    "buttonz": {
+        "launch": "echo 'Button 1'",
+        "build" : "echo 'Button 2'"
+    }
+}
+```
+If nothing is specified, a default echo command is executed.
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+RAS
+
+---
---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
+
+function runCommandInTerminal(terminalName :string, command :string) {
+    // Find an existing terminal with the specified name
+    const existingTerminal = vscode.window.terminals.find(terminal => terminal.name === terminalName);
+
+    // Check if the terminal exists
+    if (existingTerminal) {
+        existingTerminal.show();
+        existingTerminal.sendText(command);
+    } else {
+        // If the terminal doesn't exist, create a new one
+        const newTerminal = vscode.window.createTerminal({ name: terminalName });
+        newTerminal.show();
+        newTerminal.sendText(command);
+    }
+}
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    // Access settings from settings.json
+    const config = vscode.workspace.getConfiguration('buttonz');
+    const command1 = config.get('launch', 'echo "launch"');
+    const command2 = config.get('build', 'echo "build"');
+    const command3 = config.get('clean', 'echo "clean"');
+
+    // Define terminals
+    const launchTerminal = 'Launch';
+    const buildTerminal = 'Build';
+    const cleanTerminal = 'Clean';
+
+    // Register command handlers
+    vscode.commands.registerCommand('buttonz.launch', () => {
+        runCommandInTerminal(launchTerminal, command1);
+    });
+
+    vscode.commands.registerCommand('buttonz.build', () => {
+        runCommandInTerminal(buildTerminal, command2);
+    });
+
+    vscode.commands.registerCommand('buttonz.clean', () => {
+        runCommandInTerminal(cleanTerminal, command3);
+    });
+}
+
